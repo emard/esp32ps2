7c1245dd6196b0b92f46014c2518d666e1ce173c
emard <vordah@gmail.com>
2020-01-04 01:30:47 +0100

    ps2 cleanup

--- ps2.py
+++ ps2.py
@@ -99,11 +99,12 @@ class ps2:
     wlan = network.WLAN(network.STA_IF)
     ip = wlan.ifconfig()[0]
     print(ip)
-    s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
     s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1) 
     s.bind((ip,port))
     print('waiting....')
     while True:
       data,addr=s.recvfrom(1024)
-      s.sendto(data,addr)
+      #s.sendto(data,addr)
+      self.ps2write(data)
       print('received:',data,'from',addr)
