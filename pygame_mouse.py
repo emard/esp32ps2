5aabc2032771a06906e269892289d833ed1ecebd
emard <vordah@gmail.com>
2020-01-09 00:06:54 +0100

    pygame_mouse.py: support wheel and no-wheel mouse

--- pygame_mouse.py
+++ pygame_mouse.py
@@ -4,11 +4,16 @@ import pygame
 import struct
 import socket
 
-def mouse_report(dx,dy,dz,btn_left,btn_middle,btn_right):
+def mouse_wheel_report(dx,dy,dz,btn_left,btn_middle,btn_right):
   return struct.pack(">BBBB", (btn_left & 1) + ((btn_right & 1)<<1) + ((btn_middle & 1)<<2), dx & 0xFF, (-dy) & 0xFF, (-dz) & 0x0F)
+
+def mouse_nowheel_report(dx,dy,btn_left,btn_middle,btn_right):
+  return struct.pack(">BBB", (btn_left & 1) + ((btn_right & 1)<<1) + ((btn_middle & 1)<<2), dx & 0xFF, (-dy) & 0xFF)
 
 tcp_host = "192.168.48.128"
 tcp_port = 3252
+mouse_wheel = True
+
 ps2_tcp=socket.create_connection((tcp_host, tcp_port))
 print("Sending mouse events to %s:%s" % (tcp_host,tcp_port))
 ps2_tcp.sendall(bytearray([0xAA, 0x00, 0xFA]))
@@ -22,6 +27,8 @@ pygame.display.set_caption(u'Mouse events - any key to quit')
 pygame.display.flip()
 pygame.event.set_grab(True)
 pygame.mouse.set_visible(False)
+
+
 
 while(True):
   event = pygame.event.wait()
@@ -37,6 +44,14 @@ while(True):
   (dx, dy) = pygame.mouse.get_rel()
   dz = wheel
   (btn_left, btn_middle, btn_right) = pygame.mouse.get_pressed()
-  report = mouse_report(dx, dy, dz, btn_left, btn_middle, btn_right)
-  ps2_tcp.sendall(bytearray(report))
-  print("0x%08X: X=%4d, Y=%4d, Z=%2d, L=%2d, M=%2d, R=%2d" % (struct.unpack("I",report)[0], dx, dy, dz, btn_left, btn_middle, btn_right))
+
+  if mouse_wheel:
+    # mouse with wheel
+    report = mouse_wheel_report(dx, dy, dz, btn_left, btn_middle, btn_right)
+    ps2_tcp.sendall(bytearray(report))
+    print("0x%08X: X=%4d, Y=%4d, Z=%2d, L=%2d, M=%2d, R=%2d" % (struct.unpack("I",report)[0], dx, dy, dz, btn_left, btn_middle, btn_right))
+  else:
+    # mouse without wheel
+    report = mouse_nowheel_report(dx, dy, btn_left, btn_middle, btn_right)
+    ps2_tcp.sendall(bytearray(report))
+    print("X=%4d, Y=%4d, L=%2d, M=%2d, R=%2d" % (dx, dy, btn_left, btn_middle, btn_right))
