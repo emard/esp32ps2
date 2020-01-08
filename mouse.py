aac1be24a7f5afd31ba7dab5adeeb3062a74f69a
emard <vordah@gmail.com>
2020-01-08 01:57:24 +0100

    mouse.py: quit at any key

--- mouse.py
+++ mouse.py
@@ -5,11 +5,15 @@ import pygame
 pygame.init()
 (width, height) = (320, 200)
 screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption(u'Mouse events - any key to quit')
 pygame.display.flip()
 pygame.event.set_grab(True)
 pygame.mouse.set_visible(False)
 
-for i in range(150):
-  pygame.event.wait()
+while(True):
+  event = pygame.event.wait()
+  if event.type == pygame.KEYDOWN:
+    print("QUIT")
+    break
   print(pygame.mouse.get_rel(), pygame.mouse.get_pressed())
   
